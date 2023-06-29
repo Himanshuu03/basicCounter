c49803866d84c77d95efafddf8332d6124ee8c31
Himanshuu03 <himanshujangra2403@gmail.com>
2023-06-29 13:29:19 +0530

    final

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-var content = document.querySelector('#Content').innerText;
-var minus = document.querySelector('#minusContent');
-minus.addEventListener('click',()=>{
-    var num = parseInt(content);
-    num = num-1;
-    content = num;
-    document.querySelector('#Content').innerText = num;
-    console.log(num);
-})
+const countValue = document.querySelector('#Content')
 
-var plus = document.querySelector('#plusContent');
-plus.addEventListener('click',()=>{
-    var num2 = parseInt(content);
-    num2 = num2+1;
-    content = num2;
-    document.querySelector('#Content').innerText = num2;
-    console.log(num2);
-})
+const increment=()=>{
+    let value = parseInt(countValue.innerText);
+    value = value+1;
+    countValue.innerText = value;
+}
+const decerment=()=>{
+    let value = parseInt(countValue.innerText);
+    value = value-1;
+    countValue.innerText = value;
+}
+const reset=()=>{
+    countValue.innerText = "0";
+}
+// Our logic
+
+// var content = document.querySelector('#Content').innerText;
+// var minus = document.querySelector('#minusContent');
+// minus.addEventListener('click',()=>{
+//     var num = parseInt(content);
+//     num = num-1;
+//     content = num;
+//     document.querySelector('#Content').innerText = num;
+//     console.log(num);
+// })
+
+// var plus = document.querySelector('#plusContent');
+// plus.addEventListener('click',()=>{
+//     var num2 = parseInt(content);
+//     num2 = num2+1;
+//     content = num2;
+//     document.querySelector('#Content').innerText = num2;
+//     console.log(num2);
+// })
